55fad5475ae332a6ce151136f723a3de2836e7e1
Sara Gallego Mendez <saragallegomendez10@gmail.com>
2025-11-09 00:41:55 +0100

    chore: exclude eslint.config.mjs from CommonJS block and treat it as ESM

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,33 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    ignores: ['eslint.config.mjs'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: { ...globals.node, ...globals.es2021 },
+    },
+  },
+
+  {
+    files: ['eslint.config.mjs'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: { ...globals.node, ...globals.es2021 },
+    },
+  },
+
+  {
+    files: ['public/**/*.js'],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.es2021 },
+    },
+  },
 ]);
