10aa6730b7f3aaabc0cbecfee94869957ab2218b
Sara <saragallegomendez10@gmail.com>
2025-11-09 21:06:30 +0100

    fix: match filter field name to priceMax in products view

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,11 @@
-const express = require('express');
-const router = express.Router();
-const Product = require('../models/Product');
-const requireAuth = require('../middlewares/requireAuth');
+var express = require('express');
 
-/* GET home page with all products. */
-router.get('/', requireAuth, async (req, res, next) => {
-  try {
-    const products = await Product.find().sort({ createdAt: -1 }).populate('owner');
-    res.render('index', {
-      title: 'Nodepop',
-      products,
-    });
-  } catch (err) {
-    next(err);
-  }
+var router = express.Router();
+
+router.get('/', function (req, res) {
+  if (!req.session?.userId) return res.redirect('/auth/login');
+
+  return res.redirect('/products');
 });
 
 module.exports = router;
