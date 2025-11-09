d7fd237eeffad5d1af64160bb4e326ec10de2ce6
Sara <saragallegomendez10@gmail.com>
2025-11-09 17:26:05 +0100

    feat: improve the homepage and product view with updated authentication and links

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/Product');
+const requireAuth = require('../middlewares/requireAuth');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  if (!req.session?.userId) {
-    return res.redirect('/auth/login');
+/* GET home page with all products. */
+router.get('/', requireAuth, async (req, res, next) => {
+  try {
+    const products = await Product.find().sort({ createdAt: -1 }).populate('owner');
+    res.render('index', {
+      title: 'Nodepop',
+      products,
+    });
+  } catch (err) {
+    next(err);
   }
-
-  res.redirect('/products');
 });
 
 module.exports = router;
